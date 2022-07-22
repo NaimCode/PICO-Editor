32993212ae6d8453e32fa9e57cc13add2596845f
Mahamat Naim Abdelkerim <73735806+NaimCode@users.noreply.github.com>
2022-07-22 20:30:47 +0100

    refactoring onEditText

--- src/Parts/Board/shapeItem.tsx
+++ src/Parts/Board/shapeItem.tsx
@@ -95,108 +95,9 @@ const ShapeItem = ({
     <>
       {node.type == "text" && (
         <Text
-          onDblClick={() => {
-            trRef.current.hide();
-            shapeRef.current.hide();
-            const textPosition = shapeRef.current.absolutePosition();
-            const areaPosition = {
-              x: stageRef.current.container().offsetLeft + textPosition.x,
-              y: stageRef.current.container().offsetTop + textPosition.y + 68,
-            };
-            const textnode = shapeRef.current;
-            const textarea = document.createElement("textarea");
-            
-            document.body.appendChild(textarea);
-            textarea.value = props.text;
-            textarea.style.position = "absolute";
-            textarea.style.top = areaPosition.y + "px";
-            textarea.style.left = areaPosition.x + "px";
-            //textarea.style="border-[2px] border-[#00a1ff]"
-            textarea.style.width =
-              textnode.width() - textnode.padding() * 2 + "px";
-            textarea.style.height =
-              textnode.height() - textnode.padding() * 2 + "px";
-            textarea.style.fontSize = textnode.fontSize() + "px";
-            textarea.style.background = "none";
-            textarea.style.border = "none";
-
-            textarea.style.padding = "0px";
-            textarea.style.margin = "0px";
-            textarea.style.overflow = "hidden";
-            textarea.style.outline = "none";
-            textarea.style.resize = "none";
-            textarea.style.fontStyle = textnode.fontStyle();
-            textarea.style.fontWeight = textnode.fontVariant();
-            textarea.style.lineHeight = textnode.lineHeight();
-            textarea.style.letterSpacing = textnode.letterSpacing();
-            textarea.style.fontFamily = textnode.fontFamily();
-            textarea.style.transformOrigin = "left top";
-            textarea.style.textAlign = textnode.align();
-            textarea.style.color = textnode.fill();
-            const rotation = textnode.rotation();
-            let transform = "";
-            if (rotation) {
-              transform += "rotationZ(" + rotation + "deg)";
-            }
-            let px = 0;
-            const isFirefox =
-              navigator.userAgent.toLowerCase().indexOf("firefox") > -1;
-            if (isFirefox) {
-              px += 2 + Math.round(textnode.fontSize() / 20);
-            }
-            transform += "translateY(-" + px + "px)";
-            textarea.style.transform = transform;
-            textarea.style.height = "auto";
-            textarea.style.height = textarea.scrollHeight + 3 + "px";
-
-            textarea.focus();
-            function removeTextarea() {
-              textarea.parentNode?.removeChild(textarea);
-              window.removeEventListener("click", handleOutsideClick);
-              textnode.show();
-             trRef.current.show();
-              trRef.current.forceUpdate();
-            }
-
-            function setTextareaWidth(newWidth: number | undefined) {
-              if (!newWidth) {
-                newWidth = textnode.placeholder.length + textnode.fontSize();
-              }
-              newWidth = (newWidth || 0) + 1;
-              textarea.style.width = newWidth + "px";
-
-              // let isSafari '/^((?!chrome|android).)*safari/i'.test(navigator.userAgent)
-            }
-            textarea.addEventListener("keydown", function (e) {
-              if (e.key === "13" && !e.shiftKey) {
-              
-                onChange(index, {text:textarea.value
-                });
-                removeTextarea();
-              }
-              if (e.code === "27") {
-                removeTextarea();
-              }
-            });
-            textarea.addEventListener("keydown", function (e) {
-              const scale = textnode.getAbsoluteScale().x;
-              setTextareaWidth(textnode.width() + scale);
-              textarea.style.height = "auto";
-              textarea.style.height =
-                textarea.scrollHeight + textnode.fontSize() + "px";
-            });
-            function handleOutsideClick(e: any) {
-              if (e.target !== textarea) {
-
-                onChange(index, {text:textarea.value
-                });
-                removeTextarea();
-              }
-            }
-            setTimeout(() => {
-              window.addEventListener('click',handleOutsideClick)
-            });
-          }}
+          onDblClick={() =>
+            onEditText({ trRef, shapeRef, stageRef, props, index, onChange })
+          }
           {...shapeProps}
         />
       )}
@@ -269,4 +170,106 @@ const ImageShape = ({ props }: { props: any }) => {
   ) : (
     <Rect />
   );
+};
+
+const onEditText = ({
+  trRef,
+  shapeRef,
+  stageRef,
+  props,
+  index,
+  onChange,
+}: any) => {
+  trRef.current.hide();
+  shapeRef.current.hide();
+  const textPosition = shapeRef.current.absolutePosition();
+  const areaPosition = {
+    x: stageRef.current.container().offsetLeft + textPosition.x,
+    y: stageRef.current.container().offsetTop + textPosition.y + 68,
+  };
+  const textnode = shapeRef.current;
+  const textarea = document.createElement("textarea");
+
+  document.body.appendChild(textarea);
+  textarea.value = props.text;
+  textarea.style.position = "absolute";
+  textarea.style.top = areaPosition.y + "px";
+  textarea.style.left = areaPosition.x + "px";
+  //textarea.style="border-[2px] border-[#00a1ff]"
+  textarea.style.width = textnode.width() - textnode.padding() * 2 + "px";
+  textarea.style.height = textnode.height() - textnode.padding() * 2 + "px";
+  textarea.style.fontSize = textnode.fontSize() + "px";
+  textarea.style.background = "none";
+  textarea.style.border = "none";
+
+  textarea.style.padding = "0px";
+  textarea.style.margin = "0px";
+  textarea.style.overflow = "hidden";
+  textarea.style.outline = "none";
+  textarea.style.resize = "none";
+  textarea.style.fontStyle = textnode.fontStyle();
+  textarea.style.fontWeight = textnode.fontVariant();
+  textarea.style.lineHeight = textnode.lineHeight();
+  textarea.style.letterSpacing = textnode.letterSpacing();
+  textarea.style.fontFamily = textnode.fontFamily();
+  textarea.style.transformOrigin = "left top";
+  textarea.style.textAlign = textnode.align();
+  textarea.style.color = textnode.fill();
+  const rotation = textnode.rotation();
+  let transform = "";
+  if (rotation) {
+    transform += "rotationZ(" + rotation + "deg)";
+  }
+  let px = 0;
+  const isFirefox = navigator.userAgent.toLowerCase().indexOf("firefox") > -1;
+  if (isFirefox) {
+    px += 2 + Math.round(textnode.fontSize() / 20);
+  }
+  transform += "translateY(-" + px + "px)";
+  textarea.style.transform = transform;
+  textarea.style.height = "auto";
+  textarea.style.height = textarea.scrollHeight + 3 + "px";
+
+  textarea.focus();
+  function removeTextarea() {
+    textarea.parentNode?.removeChild(textarea);
+    window.removeEventListener("click", handleOutsideClick);
+    textnode.show();
+    trRef.current.show();
+    trRef.current.forceUpdate();
+  }
+
+  function setTextareaWidth(newWidth: number | undefined) {
+    if (!newWidth) {
+      newWidth = textnode.placeholder.length + textnode.fontSize();
+    }
+    newWidth = (newWidth || 0) + 1;
+    textarea.style.width = newWidth + "px";
+
+    // let isSafari '/^((?!chrome|android).)*safari/i'.test(navigator.userAgent)
+  }
+  textarea.addEventListener("keydown", function (e) {
+    if (e.key === "13" && !e.shiftKey) {
+      onChange(index, { text: textarea.value });
+      removeTextarea();
+    }
+    if (e.code === "27") {
+      removeTextarea();
+    }
+  });
+  textarea.addEventListener("keydown", function (e) {
+    const scale = textnode.getAbsoluteScale().x;
+    setTextareaWidth(textnode.width() + scale);
+    textarea.style.height = "auto";
+    textarea.style.height = textarea.scrollHeight + textnode.fontSize() + "px";
+  });
+  function handleOutsideClick(e: any) {
+    if (e.target !== textarea) {
+      onChange(index, { text: textarea.value });
+      removeTextarea();
+    }
+  }
+  setTimeout(() => {
+    window.addEventListener("click", handleOutsideClick);
+  });
 };
