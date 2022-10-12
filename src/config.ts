2917f21840bb9df26025c6a80308f08cce4bb67b
Naim abdelkerim mahamat <n.mahamat@isicod.net>
2022-10-12 16:09:44 +0100

    resolve

--- src/config.ts
+++ src/config.ts
@@ -5,26 +5,26 @@ export const gf = new GiphyFetch(KEY_GIPHY);
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getFirestore } from 'firebase/firestore/lite';
-import { getAnalytics } from "firebase/analytics";
+
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_APP_API_KEY, // "AIzaSyBy9Rgt16ePT52Del6rOuoq_BdUIBIPeDU",
-  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN, //"pico-ec9d2.firebaseapp.com",
-  projectId: import.meta.env.VITE_APP_PROJECT_ID, //"pico-ec9d2",
-  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET, //"pico-ec9d2.appspot.com",
-  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SEND_ID, //"931319819485",
-  appId: import.meta.env.VITE_APP_APP_ID, /// "1:931319819485:web:b0f787e9d6136928f9bf34",
-  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID, ///"G-702FZTVGPL"
+  apiKey: import.meta.env.VITE_APP_API_KEY, 
+  authDomain: import.meta.env.VITE_APP_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_APP_PROJECT_ID, 
+  storageBucket: import.meta.env.VITE_APP_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_APP_MESSAGING_SEND_ID,
+  appId: import.meta.env.VITE_APP_APP_ID, 
+  measurementId: import.meta.env.VITE_APP_MEASUREMENT_ID, 
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+
 const db=getFirestore(app);
 
-export { app, analytics, db };
+export { app, db };
 
